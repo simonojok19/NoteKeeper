6c6906750827029e1865dad7588c3bbba29009f8
simonojok19 <simonojok19@gmail.com>
2020-07-18 18:55:16 +0300

    added another course

--- app/src/main/java/com/example/notekeeper/DataManager.kt
+++ app/src/main/java/com/example/notekeeper/DataManager.kt
@@ -3,4 +3,11 @@ package com.example.notekeeper
 class DataManager {
     val courses = HashMap<String, CourseInfo>()
     val notes = ArrayList<NoteInfo>()
+
+    private fun initializeCourses() {
+        var course = CourseInfo("android_intent", "Android Programming with Intents")
+        courses.set(course.courseId, course)
+        course = CourseInfo(courseId = "android_async", title = "Android Async Programming and Services")
+        courses.set(key = course.courseId, value = course)
+    }
 }
