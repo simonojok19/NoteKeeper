d2c8f36964b88285a215e0f714ee6fda21ca51d5
simonojok19 <simonojok19@gmail.com>
2020-07-18 19:58:27 +0300

    added initializer block

--- app/src/main/java/com/example/notekeeper/DataManager.kt
+++ app/src/main/java/com/example/notekeeper/DataManager.kt
@@ -4,10 +4,18 @@ class DataManager {
     val courses = HashMap<String, CourseInfo>()
     val notes = ArrayList<NoteInfo>()
 
+    init {
+        initializeCourses()
+    }
+
     private fun initializeCourses() {
         var course = CourseInfo("android_intent", "Android Programming with Intents")
-        courses.set(course.courseId, course)
+        courses[course.courseId] = course
         course = CourseInfo(courseId = "android_async", title = "Android Async Programming and Services")
         courses.set(key = course.courseId, value = course)
+        course = CourseInfo(title = "Java_Fundamental", courseId = "java_lang")
+        courses.set(value = course, key = course.courseId)
+        course = CourseInfo(courseId = "java_core", title = "Java Fundamentals: The core Platform")
+        courses[course.courseId] = course
     }
 }
